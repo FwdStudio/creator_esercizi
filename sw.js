b6c4d65ceff974ba04de27366ff753e18ae60230
FwdStudio <gastronarrativa@gmail.com>
2026-08-02 10:36:19 +0200

    Rename cache from 'editor-esercizi-v1' to 'coach-draw-v1'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'editor-esercizi-v1';
+const CACHE_NAME = 'coach-draw-v1';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
